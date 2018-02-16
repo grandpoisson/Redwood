83db1c45ef06cd8e67914782509875e55a588786
grandpoisson <grandpoisson@gmail.com>
2018-02-16 16:49:57 +0400

    changes to mailer headers

--- mail_handler.php
+++ mail_handler.php
@@ -1,20 +1,23 @@
 <?php 
 if(isset($_POST['submit'])){
-    echo"Starting mail handler";
     $to = "steve.codd@emirates.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $full_name = $_POST['full_name'];
     $subject = "Form submission";
     $subject2 = "Copy of your form submission";
     $message = $full_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $full_name . "\n\n" . $_POST['message'];
+    
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    echo "Mail composed and next we will call mail() function";
-    mail($to,$subject,$message,$headers);
-    echo "Mail sent to TO recipient";
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    $headers = "From:" . $from . "\r\n" .
+    'Reply-To: info@redwood-formations.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+    if ( mail($to,$subject,$message,$headers) ) {
+        echo "The email has been sent!";
+        } 
+        else {
+        echo "The email has failed!";
+        }
+   
     echo "Mail Sent. Thank you " . $full_name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
